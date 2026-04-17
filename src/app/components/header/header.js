4f26c6100221e6b94169eaa2ac14b468398c97dc
Hiraina-RND <hei.hiraina@gmail.com>
2026-04-17 18:06:19 +0300

    feat: add header

---
+++ src/app/components/header/header.js
@@ -0,0 +1,20 @@
+import headerCss from "./header.module.css"
+
+const Header = () => {
+    return (
+            <header className={headerCss.header}>
+                <div className={headerCss.headerRow}>
+                    <h1 className={headerCss.title}>Recipe Book</h1>
+                    <button
+                        type="button"
+                        className={headerCss.headerToggleBtn}
+                        // onClick={handleToggleOrder}
+                    >
+                        Reverse order
+                    </button>
+                </div>
+            </header>
+    );
+}
+
+export default Header;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,9 @@
+import Header from "./components/header/header";
+
 export default function Home() {
   return (
-    <h1>recipe-app</h1>
+    <>
+      <Header />
+    </>
   );
 }
