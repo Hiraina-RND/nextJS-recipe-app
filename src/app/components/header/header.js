08662a3fa6bbfb59fac34fa70ea09025b1248823
Hiraina-RND <hei.hiraina@gmail.com>
2026-04-18 08:06:14 +0300

    feat: add recipe search and Font Awesome icons

--- src/app/components/header/header.js
+++ src/app/components/header/header.js
@@ -1,10 +1,21 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import headerCss from "./header.module.css"
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
-const Header = ({ handleToggleOrder }) => {
+const Header = ({ handleToggleOrder, findRecipeByName }) => {
     return (
             <header className={headerCss.header}>
                 <div className={headerCss.headerRow}>
                     <h1 className={headerCss.title}>Recipe Book</h1>
+                    <div className={headerCss.searchBar}>
+                        <input
+                        className={headerCss.searchInput}
+                        type="text"
+                        placeholder="Search a recipe by name . . ."
+                        onChange={(e) => findRecipeByName(e.target.value)}
+                        />
+                        <FontAwesomeIcon className={headerCss.searchIcon} icon={faSearch} />
+                    </div>
                     <button
                         type="button"
                         className={headerCss.headerToggleBtn}
--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,31 @@
 "use client";
 
 import { useState } from "react";
-import recipe from "../data/data.json"
+import recipes from "../data/data.json"
 import Header from "./components/header/header";
 import RecipeList from "./components/recipe/recipeList/recipeList";
 import homeCss from "./page.module.css"
 
 export default function Home() {
-  const [orderRecipes, setOrderRecipes] = useState(recipe)
+  const [orderRecipes, setOrderRecipes] = useState(recipes);
+  const [searchValue, setSearchValue] = useState("");
+  const filteredRecipes = orderRecipes.filter((recipe) =>
+    recipe.name.toLowerCase().includes(searchValue.toLowerCase())
+  );
 
   const handleToggleOrder = () => {
     setOrderRecipes((prev) => [...prev].reverse())
   }
 
+  const findRecipeByName = (value) => {
+    setSearchValue(value);
+  }
+
   return (
     <div className={homeCss.app}>
-      <Header handleToggleOrder={handleToggleOrder} />
+      <Header handleToggleOrder={handleToggleOrder} findRecipeByName={findRecipeByName} />
       <main className={homeCss.main}>
-        <RecipeList recipes={orderRecipes} />
+        <RecipeList recipes={filteredRecipes} />
       </main>
     </div>
   );
